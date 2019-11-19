6ee86c3574689de489fed197247abbf9d3b9d646
Loic Lissens <loiclissens@MacBook-Pro-de-Loic.local>
2019-11-19 10:47:08 +0100

    read me et fin de projet

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -22,6 +22,7 @@ let dataYearsTop = []; // tab ou sont stockés les  années
 const getTableTop = document.querySelector("#table1");
 let getDataCountryTop = getTableTop.querySelectorAll("tr");
 getDataCountryTop = [...getDataCountryTop];
+
 // Recuperation des données des pays
 for (let i=2; i<getDataCountryTop.length;i++){
     dataCountryTop.push(getDataCountryTop[i].textContent);
@@ -46,12 +47,18 @@ for (let i = 0; i<dataCountryTop.length; i++){
 dataYearsTop.push(getDataCountryTop[1].textContent);
 dataYearsTop[0] = dataYearsTop[0].replace(/\s+/g, ' ').trim();
 dataYearsTop = dataYearsTop[0].split(" ");
-
-
 //////////////////////////////////////////////////////////////////////////////
-// CREATION DU CANVAS ET GRAPHIQUES 
 
-let dataCountryOne = dataCountryTop[0];
+// CREATION DU CANVAS ET GRAPHIQUES 
+function startgraph () {
+let chosenCountry = d3.select("#selectmenu")
+                        .property("value");
+
+let find = countryNameTop.findIndex(x => x == chosenCountry);
+
+let indice = find;
+let dataCountryOne = dataCountryTop[indice];
+
 const min = d3.min(dataCountryOne, d => d);
 const max = d3.max(dataCountryOne, d => d);
 
@@ -88,11 +95,21 @@ const rectanglesTop  = graphTop.selectAll("rect")
                         .data(dataCountryOne)
                         .enter()
                         .append("rect")
-                        .attr("fill","orange")
+                        .attr("fill","coral")
                         .attr("width",50)
                         .attr("height", (d) => 450 - y(d)) // 450 pour qu'il commence sur l'axis X
                         .attr("x", function (d,i){return x(dataYearsTop[i])})
-                        .attr("y", (d) => y(d));
+                        .attr("y", (d) => y(d))
+                        .on("mouseover", function(d,i){
+                            d3.select(this).attr("fill", "orange")
+                                            .attr("stroke","coral")
+                                            .attr("stroke-width","3");
+                            })
+                        .on("mouseout", function(d,i){
+                            d3.select(this).attr("fill", "coral")
+                                            .attr("stroke","coral")
+                            .               attr("stroke-width","0");
+                        });
                         
 const axeX = d3.axisBottom(x); // création de l'axe pour les dates
 const axeY = d3.axisLeft(y)  // création de l'axe pour les valeurs de crimes
@@ -104,7 +121,246 @@ groupeX.call(axeX) // appel de l'axe des dates dans le grp X
 groupeY.call(axeY) //appel de l'axe des valeurs dans le grp Y
         .style("font-size", "11px");
 
+ d3.select("#selectmenu").on("change", () => {
+    svgTop.remove();
+    startgraph();
+ })
+ 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Menu dérouant 
+const selectmenu = d3.select("caption")
+                .insert("select","svg")
+                .attr("id", "selectmenu")
+                .style("margin-left","50px");
+                        
+        selectmenu.selectAll("option")
+                    .data(countryNameTop)
+                    .enter()
+                    .append("option")
+                    .text(function(d){return d}) 
+                    .attr("valeur", function(d){return d});
+
+ startgraph();
+///////////////////////////////////////////////////////////////////////////
+// TABLEAU DU BAS 
+// Récupération des données
+let countryNameBottom = []; // tab ou sont stocké les noms des pays du tab  bottom
+let dataCountryBottom = []; // tab ou sont stockés les data par pays du bottom
+let dataYearsBottom = []; // tab ou sont stockés les  années bottom
+
+const getTableBottom = document.querySelector("#table2");
+let getDataCountryBottom = getTableBottom.querySelectorAll("tr");
+getDataCountryBottom = [...getDataCountryBottom];
+
+// Recuperation des données des pays du tab du bas
+for (let i=1; i<getDataCountryBottom.length;i++){
+    dataCountryBottom.push(getDataCountryBottom[i].textContent);
+}
+
+for(let i=0; i<dataCountryBottom.length;i++){
+    dataCountryBottom[i] = dataCountryBottom[i].replace(/\s+/g, ' ').trim();
+    dataCountryBottom[i] = dataCountryBottom[i].split(" ");
+    dataCountryBottom[i].shift();
+    for(let l = 1; l<dataCountryBottom[i].length;l++ ){
+       
+        dataCountryBottom[i][l] =  parseFloat(dataCountryBottom[i][l]);
+       }
+}
+for (let i = 0; i<dataCountryBottom.length; i++){
+    countryNameBottom.push(dataCountryBottom[i][0]);
+    dataCountryBottom[i].shift();
+}
+
+// Récupération des dates du tableau du bas
+dataYearsBottom.push(getDataCountryBottom[0].textContent);
+dataYearsBottom[0] = dataYearsBottom[0].replace(/\s+/g, ' ').trim();
+dataYearsBottom = dataYearsBottom[0].split(" ");
+dataYearsBottom.shift();
+dataYearsBottom.shift();
+dataCountryBottom[7].shift();
+
+///////////////////////////////////////////////////////////////////////////
+// GRAPHIQUE DU BAS
+
+function startgraphbottom () {
+
+let chosenCountryBottom = d3.select("#selectmenuBottom")
+    .property("value");
+
+let findBottom = countryNameBottom.findIndex(v => v == chosenCountryBottom);
+
+let indiceBottom = findBottom;
+
+let dataCountryTwo = dataCountryBottom[indiceBottom];
+
+const minB = d3.min(dataCountryTwo, d => d);
+const maxB = d3.max(dataCountryTwo, d => d);
+
+const marginB = {top: 20, right: 20, bottom : 100, left : 100};
+const graphWB = 800 - marginB.left - marginB.right;
+const graphHB = 500 - marginB.top - marginB.bottom;
+const yB = d3.scaleLinear()
+            .domain([0, maxB])
+            .range([450,0]);
+
+const xB = d3.scaleBand()
+            .domain(dataYearsBottom)
+            .range([0, 650])
+            .paddingInner(0.2)
+            .paddingOuter(0.3);
+
+const svgBottom = d3.select("#Homicides")
+                .append("svg")
+                .attr("width", 800)
+                .attr("height", 500)
+                .style("background-color","white");
+
+const graphBottom = svgBottom.append("g") // groupe avec tout les battonet dedans
+                .attr("widht", graphWB)
+                .attr("height",graphHB)
+                .attr("transform",`translate(${marginB.left},${marginB.top})`); 
+
+const groupeXB = graphBottom.append("g")
+                .attr("transform", `translate(0,450)`); // translate de 450, la ou doit etre l'axis x
+
+const groupeYB = graphBottom.append("g");
+
+const rectanglesBottom  = graphBottom.selectAll("rect")
+                        .data(dataCountryTwo)
+                        .enter()
+                        .append("rect")
+                        .attr("fill","coral")
+                        .attr("width",200)
+                        .attr("height", (d) => 450 - yB(d)) // 450 pour qu'il commence sur l'axis X
+                        .attr("x", function (d,i){return xB(dataYearsBottom[i])})
+                        .attr("y", (d) => yB(d))
+                        .on("mouseover", function(d,i){
+                            d3.select(this).attr("fill", "orange")
+                                            .attr("stroke","coral")
+                                            .attr("stroke-width","3");
+                            })
+                        .on("mouseout", function(d,i){
+                            d3.select(this).attr("fill", "coral")
+                                            .attr("stroke","coral")
+                            .               attr("stroke-width","0");
+                        });
+                        
+const axeXB = d3.axisBottom(xB); // création de l'axe pour les dates
+const axeYB = d3.axisLeft(yB)  // création de l'axe pour les valeurs de crimes
+               .ticks(20)
+               .tickFormat(d => d + " (en milliers)");
+                        
+groupeXB.call(axeXB) // appel de l'axe des dates dans le grp X
+       .style("font-size", "14px");
+groupeYB.call(axeYB) //appel de l'axe des valeurs dans le grp Y
+        .style("font-size", "11px");
+d3.select("#selectmenuBottom").on("change", () => {
+            svgBottom.remove();
+            startgraphbottom();
+         })
+
+}
+
+const selectmenuBottom = d3.select("#Homicides")
+                .insert("select","svg")
+                .attr("id", "selectmenuBottom")
+                .style("margin-left","50px");
+                        
+        selectmenuBottom.selectAll("option")
+                    .data(countryNameBottom)
+                    .enter()
+                    .append("option")
+                    .text(function(d){return d}) 
+                    .attr("valeur", function(d){return d});
+
+ startgraphbottom();
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  GRAPHIQUE LIVE  TOUT SAUF TERMINER 
+ 
+
+   
+/*window.addEventListener("load", async() =>{
+
+   
+    
+    try {
+        const requete = await fetch("https://inside.becode.org/api/v1/data/random.json");
+        const data = await requete.json();
+        console.log(data);
+
+        const getHOne =  d3.select("#firstHeading")
+        .append("svg")
+        .attr("width", 800)
+        .attr("height", 500)
+        .style("background-color","red");
+
+        const minLive = d3.min(data, d => d);
+        const maxLive = d3.max(data, d => d);
+
+        const marginLive = {top: 20, right: 20, bottom : 100, left : 100};
+        const graphWLive = 800 - marginLive.left - marginLive.right;
+        const graphHLive = 500 - marginLive.top - marginLive.bottom;
+        const yLive = d3.scaleLinear()
+            .domain([minLive, maxLive])
+            .range([450,0]);
+
+        const xLive = d3.scaleBand()
+            .domain(data[1])
+            .range([0, 650])
+            .paddingInner(0.2)
+            .paddingOuter(0.3);
+
+        
+
+const graphLive = getHOne.append("g") // groupe avec tout les battonet dedans
+                .attr("widht", graphWLive)
+                .attr("height",graphHLive)
+                .attr("transform",`translate(${marginLive.left},${marginLive.top})`); 
+
+const groupeXLive = getHOne.append("g")
+                .attr("transform", `translate(0,450)`); // translate de 450, la ou doit etre l'axis x
+
+const groupeYLive = getHOne.append("g");
+
+const rectanglesLive  = graphLive.selectAll("rect")
+                        .data(data[0])
+                        .enter()
+                        .append("rect")
+                        .attr("fill","coral")
+                        .attr("width",50)
+                        .attr("height", (d) => 450 - yLive(d)) // 450 pour qu'il commence sur l'axis X
+                        .attr("x", function (d,i){return d })
+                        .attr("y", (d) => yLive(d))
+                        .on("mouseover", function(d,i){
+                            d3.select(this).attr("fill", "orange")
+                                            .attr("stroke","coral")
+                                            .attr("stroke-width","3");
+                            })
+                        .on("mouseout", function(d,i){
+                            d3.select(this).attr("fill", "coral")
+                                            .attr("stroke","coral")
+                            .               attr("stroke-width","0");
+                        });
+                        
+const axeXLive = d3.axisBottom(axeXLive); // création de l'axe pour les dates
+const axeYLive = d3.axisLeft(yLive)  // création de l'axe pour les valeurs de crimes
+               .ticks(20);
+               
+                        
+groupeXLive.call(axeX) // appel de l'axe des dates dans le grp X
+       .style("font-size", "14px");
+groupeYLive.call(axeYLive) //appel de l'axe des valeurs dans le grp Y
+        .style("font-size", "11px");
+        
+    } catch(error) {
+        console.error(error);
+    }
+   
 
 
+});*/
 
-
+    
--- readme.md
+++ readme.md
@@ -1,15 +1,20 @@
-# Titre du projet
+# Data visualisation avec D3 js
+
+Début du projet:  ??
+Fin du projet : Mardi 19 Novembre 2019.
 
 ## Description du projet
 
-Date / le projet/ initiateur /but 
+Exercice visant à récupérer des données d'une API ou d'une page HTML en parcourant le DOM, les traitées et les ressortire sous forme de graphique.
+Et cela grace à la biblihotèque d3 js.
 
 ## Technologie utilisés
-
+Javascript vanilla et d3 js
 ## Contenu (si il y a)
-
+Des graphiques adaptatifs en fonction de données.
 ## Progression 
-
+    80 % a la date de fin du projet il me reste le graphique des données fetché de l'API becode à traité ce qui n'a pas su etre fait par manque de temps.
 ## Contributeurs
-
+Loïc Lissens
 ## Remerciements 
+Les membres de la promotion Woods 2.15 pour leur aide lors de certains problèmes.
